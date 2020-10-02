fddef89ccf9f34b07f339f027524582d9f2ebde3
TheoAcker12 <43011398+TheoAcker12@users.noreply.github.com>
2020-10-02 16:06:46 -0500

    flair and awards

--- main_to_csv.py
+++ main_to_csv.py
@@ -19,9 +19,9 @@ def main():
     # set up variables
     commentList = []
     submissionList = []
+    awardList = []
 
     # loop through submissions
-    # TODO: Determine what submissions to scrape
     
     # REMEMBER: Make an empty "raw_comics" folder before running
     for submission in api.search_submissions(before=start, after=end, subreddit="polandball", limit=10):
@@ -32,11 +32,18 @@ def main():
         for comment in submission.comments:
             #commentList.append(getCommentData(comment))  ##Gets comment data without finding child_comments
             commentList.extend(loopComment(comment, []))
+        # submission awards
+        if (submission.total_awards_received > 0): awardList.extend(getAwardData(submission))
+        # comment awards
+        for comment in submission.comments.list():
+            if (comment.total_awards_received > 0): awardList.extend(getAwardData(comment))
             
     df1 =pandas.DataFrame(submissionList) 
     df1.to_csv("submissionList.csv")
     df2 =pandas.DataFrame(commentList) 
     df2.to_csv("commentList.csv")
+    df3 = pandas.DataFrame(awardList)
+    df3.to_csv("awardList.csv")
 
 def getSubmissionData(submission):
     record = { #title of each column in our xlsx file
@@ -59,8 +66,8 @@ def getSubmissionData(submission):
     saveImage(submission)
     
     record = getAuthorPremium(submission, record)
-    
     record = getAuthorFlair(submission, record)
+    record = getAwards(submission, record)
     
     return record
     
@@ -90,30 +97,28 @@ def getCommentData(comment):
         "is_controversial": comment.controversiality,
         "is_edited": comment.edited,
         "upvotes": comment.score,
-        #"total_awards": comment.total_rewards_received,
         "gold": comment.gilded,
         "url": comment.permalink,
         "body": comment.body
     }
     
-    record = getAuthorPremium(comment, record)
-    
-    record = getAuthorFlair(comment, record)
+    record = getAuthorPremium(comment, record)  
+    record = getAuthorFlair(comment, record)    
+    record = getAwards(comment, record)
     
     return record
 
-def getAuthorFlair(item, d):
-    # TODO: Decide what info to collect for author flair
-    # This is actually somewhat complicated as the flair for honorary winged hussars is different
-     
+def getAuthorFlair(item, d):     
     try:
         d["author_flair_name"] = item.author_flair_richtext[0]['a'] # the name of the emoji
         d["author_flair_text"] = item.author_flair_richtext[-1]['t'].strip() # the text that goes with the flair
         d["author_flair_url"] = item.author_flair_richtext[0]['u'] # the url for the flair
+        d["author_hussar"] = (len(item.author_flair_richtext) > 2)
     except:
         d["author_flair_name"] = ""
         d["author_flair_text"] = ""
         d["author_flair_url"] = ""
+        d["author_hussar"] = False
     return d
 
 def getAuthorPremium(item, d):
@@ -122,6 +127,28 @@ def getAuthorPremium(item, d):
     except:
         d["author_premium"] = ""
     return d
+
+def getAwards(item, d):
+    # total number received
+    d["total_awards"] = item.total_awards_received
+    # value of all awards
+    value = 0
+    for award in s.all_awardings:
+        value = value + (award['count']*award['coin_price'])
+    d["total_award_value"] = value
+    return d
+
+def getAwardData(item):
+    awardList = []
+    for award in item.all_awardings:
+        record = {
+            "item_id": item.fullname,
+            "award_name": award['name'],
+            "award_count": award['count'],
+            "award_price": award['coin_price']
+        }
+        awardList.append(record)
+    return awardList
 
 def loopComment(comment, commentList):
     x = 0 # for counting all child comments
