c8f36cffd605c3ef75a1eabff6471a66fc08d3e1
TheoAcker12 <43011398+TheoAcker12@users.noreply.github.com>
2021-02-26 14:09:47 -0600

    Update extensionlessImage.py

--- extensionlessImage.py
+++ extensionlessImage.py
@@ -1,45 +1,53 @@
-# It is probably possible to do this better, but this at least seems to work reasonably well
+# Option for all at once?
 
 import pandas as pd
 import requests
+from PIL import Image
+
 # Open up the file we will be working with (as a dataframe)
 df = pd.read_csv("./data/2012 data/submissionList2012.csv")
 
-# submission urls that were not deleted, did not have an extension, and were not from imgur
-otherList = []
-# file names of images we downloaded (to check)
-checkList = []
+otherList = [] # submission urls that were not deleted, did not have an extension, and were not from imgur
+# checkList = [] # file names of images we downloaded (to check)
+corruptedList = [] # images that were corrupted
+newList = [] # images that were downloaded and were not corrupted
 
 # function to apply - will find all images that are missing an extension and try downloading them
 def applyFunc(row):
-    if ((row['text'] != ("[deleted]")) and (len(row['url'].split(".")[-1]) != 3)):
+    extension = row['url'].split(".")[-1]
+    if ((row['text'] != ("[deleted]")) and (len(extension) != 3)):
         # if url includes imgur, try getting the jpg
         if ("imgur.com" in row['url']):
             url = row['url'] + ".jpg"
-            name = url.split("/")[-1]
+            # name = url.split("/")[-1]
+            name = row['sub_id']
             resp = requests.get(url)
-            # Rename the folder name
-            with open("./test-images/" + row['sub_id'] + ".jpg", "wb") as f:
+            with open("./test-images/" + name + ".jpg", "wb") as f:
                 f.write(resp.content)
-            checkList.append(name)
+            # check
+            try:
+                img = Image.open("./test-images/" + name + ".jpg")
+                img.verify()
+                newList.append(name)
+                return "jpg"
+            except (IOError, SyntaxError) as e:
+                corruptedList.append(name)
+                # could possibly add code to remove file?
+                return
         else:
             otherList.append(row['url'])
+            return
+    else: # add statement to make note of extension
+        return extension
 
-df.apply(applyFunc, axis=1)
+df['extension'] = df.apply(applyFunc, axis=1)
 
-# now let's check for corrupted files
-# code based on https://opensource.com/article/17/2/python-tricks-artists
-from PIL import Image
-
-corruptedList = []
-
-for item in checkList:
-    try:
-        # try opening the file
-        img = Image.open("./test-images/" + item)
-        img.verify()
-    except (IOError, SyntaxError) as e:
-        # error - corrupted image
-        corruptedList.append(item)
-
-# If running this as a normal python file (not in Jupyter Notebook), make sure to save or print otherList and corruptedList
+# print
+print("Other List:")
+for (item in otherList): print(item)
+print("-"*60)
+print("Corrupted List:")
+for (item in corruptedList): print(item)
+print("-"*60)
+print("New List:")
+for item in (newList): print(item)
